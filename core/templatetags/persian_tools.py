7a7ee9d3f853ae7d7177122f9d9b196e31db8aab
Omid Shojaee <omid.shojaee@gmail.com>
2024-05-21 12:47:21 +0330

    Create Core app and a template filter to switch English digits to Persian digits in a string

---
+++ core/templatetags/persian_tools.py
@@ -0,0 +1,11 @@
+from django import template
+
+# Define your custom template tags and filters here
+
+register = template.Library()
+
+
+@register.filter(name='persian_digits')
+def persian_digits(value):
+    translation_table = str.maketrans("0123456789", "۰۱۲۳۴۵۶۷۸۹")
+    return value.translate(translation_table)
