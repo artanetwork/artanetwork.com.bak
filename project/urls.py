2a0eea3c0087620455011b5f2d36874d738e03e7
Omid Shojaee <omid.shojaee@gmail.com>
2024-05-21 12:12:30 +0330

    Custom user model with Jalali date, set the direction of username and password fields to LTR

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from accounts.views import CustomLoginView, CustomPasswordChangeView
+
 urlpatterns = [
+    path('admin/login/', CustomLoginView.as_view(), name='login'),
+    path('admin/password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
